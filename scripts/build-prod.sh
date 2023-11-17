49ed084e9b7f97ef1246bea036bfd23fd679042c
Lukas Jacomin <lukasjacomin@gmail.com>
2023-11-16 23:39:29 -0300

    feat: build script check prisma files

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -2,4 +2,12 @@ rimraf dist
 nest build --webpack --webpackPath ./webpack.config.js
 # nest build
 cp .env pnpm-lock.yaml package.json dist/
-# cp .env pnpm-lock.yaml package.json prisma/schema.prisma node_modules/prisma/query_engine-windows.dll.node dist/
+
+FILES=('prisma/schema.prisma' 'node_modules/prisma/query_engine-windows.dll.node')
+
+for file in "${FILES[@]}"
+do
+  if [ -f "$file" ]; then
+    cp $file dist/
+  fi
+done
