37ba196dad4bac3537215342a953b097e4a51cde
Lukas Jacomin <lukasjacomin@gmail.com>
2023-11-19 23:30:19 -0300

    fix: build script

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,13 +1,18 @@
-rimraf dist
+#!/bin/sh
+rm -rf dist
 nest build --webpack --webpackPath ./webpack.config.js
 # nest build
 cp .env pnpm-lock.yaml package.json dist/
 
-FILES=('prisma/schema.prisma' 'node_modules/prisma/query_engine-windows.dll.node')
+# BASH
+# FILES=('prisma/schema.prisma' 'node_modules/prisma/query_engine-windows.dll.node')
+# for file in "${FILES[@]}"
+# do
+#   if [ -f "$file" ]; then
+#     cp $file dist/
+#   fi
+# done
 
-for file in "${FILES[@]}"
-do
-  if [ -f "$file" ]; then
-    cp $file dist/
-  fi
-done
+[[ -e 'prisma/schema.prisma' ]] && cp 'prisma/schema.prisma' dist/
+[[ -e 'node_modules/prisma/query_engine-windows.dll.node' ]] && cp 'node_modules/prisma/query_engine-windows.dll.node' dist/
+[[ -e 'node_modules/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node' ]] && cp 'node_modules/prisma/libquery_engine-linux-musl-openssl-3.0.x.so.node' dist/
