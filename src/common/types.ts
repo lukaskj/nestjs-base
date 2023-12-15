644326d35fe9c2c390798d62ef274361c10a1ed0
Lukas Jacomin <lukasjacomin@gmail.com>
2023-12-15 01:00:14 -0300

    class type

--- src/common/types.ts
+++ src/common/types.ts
@@ -4,7 +4,7 @@ export type FunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ?
 export type NonFunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? never : K }[keyof T];
 export type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
 
-export type ClassConstructor<T> = { new (): T };
+export type ClassConstructor<T> = { new (...args: any[]): T };
 export type Self<T> = ClassConstructor<T>;
 
 export type AnyObject = { [key: string]: any };
